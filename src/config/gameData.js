e965d16805831dd44bfd5e4726e7b7c7bb3cb5b2
genstorebyepand <deandri.epan21@smp.belajar.id>
2026-05-26 17:44:58 +0700

    Create gameData.js

---
+++ src/config/gameData.js
@@ -0,0 +1,23 @@
+const brainTeasers = [
+    { question: "Aku punya 2 kaki di siang hari, 3 kaki di malam hari. Siapakah aku?", answer: "manusia" },
+    { question: "Semakin banyak kamu ambil, semakin besar aku. Apakah aku?", answer: "lubang" },
+    { question: "Apa yang bisa kamu pegang tapi tidak bisa disentuh?", answer: "janji" },
+    { question: "Apa yang selalu datang tapi tidak pernah tiba?", answer: "besok" },
+    { question: "Kota apa yang cuma punya 3 huruf?", answer: "depok" },
+    { question: "Hewan apa yang namanya terdiri dari 2 huruf?", answer: "ular" },
+    { question: "Apa yang naik tapi tidak pernah turun?", answer: "umur" },
+    { question: "Benda apa yang kalau dipegang keras, tapi kalau dilempar jadi lembut?", answer: "bantal" }
+];
+
+const ttsQuestions = [
+    { question: "Apa yang naik tapi tidak pernah turun?", answer: "umur" },
+    { question: "Benda apa yang kalau dipegang keras, tapi kalau dilempar jadi lembut?", answer: "bantal" },
+    { question: "Apa bedanya kalo sama sama?", answer: "beda" },
+    { question: "Apa yang bisa terbang tanpa sayap?", answer: "waktu" },
+    { question: "Siapa yang selalu menjadi tamu tapi tak pernah diundang?", answer: "masalah" }
+];
+
+module.exports = {
+    brainTeasers,
+    ttsQuestions
+};
